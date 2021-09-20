7b03e95e49dcf09a5e719c9e563eb84f13b70263
Lorentz-Andrea ROMEO <lorentz-andrea.romeo@epitech.eu>
2021-09-20 16:40:23 +0200

    Added main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPP-500-NCE-5-1-babel-lionel.da-rocha-da-silva
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    std::cout << "Antonin is a fion." << std::endl;
+    return (0);
+}
