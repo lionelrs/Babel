53df9fe37187a2f8f7c452446e2a7224c5dec561
Lionel Silva <lionel.da-rocha-da-silva@epitech.eu>
2021-09-23 09:31:23 +0200

    half goog version

--- conanTest/src/main.cpp
+++ conanTest/src/main.cpp
@@ -6,9 +6,17 @@
 */
 
 #include <iostream>
+#include <portaudio.h>
+#include <opus.h>
 
 int main(int ac, char **av)
 {
-    std::cout << "coucou je compile" << std::endl;
-    return (0);
+	PaError err = Pa_Initialize();
+	if (err != paNoError) {
+		return 84;
+	}
+	err = Pa_Terminate();
+	std::cout << opus_encoder_get_size(1) << std::endl;
+    	std::cout << "coucou je compile" << std::endl;
+    	return (0);
 }
