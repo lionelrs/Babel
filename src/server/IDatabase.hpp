4378d8a19eee15dcc2f92bac477a8cc33889ed46
antonin <antonin.fille@epitech.eu>
2021-10-05 18:09:55 +0200

    add IDataBase

---
+++ src/server/IDatabase.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** Untitled (Workspace)
+** File description:
+** IDatabase
+*/
+
+#ifndef IDATABASE_HPP_
+#define IDATABASE_HPP_
+
+class IDatabase {
+public:
+    ~IDatabase();
+
+protected:
+private:
+};
+
+#endif /* !IDATABASE_HPP_ */
