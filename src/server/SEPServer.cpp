e94fd165cae929819a2c437ee3595b7f00c4bbff
Marton Szuts <marton.szuts@epitech.eu>
2021-10-11 15:43:17 +0200

    Already calling

--- src/server/SEPServer.cpp
+++ src/server/SEPServer.cpp
@@ -15,7 +15,6 @@ SEPServer::SEPServer(int port)
     _cmd.emplace(450, &SEPServer::cmdCall);
     _cmd.emplace(460, &SEPServer::cmdRefuseCall);
     _cmd.emplace(470, &SEPServer::cmdCallHangUp);
-    _cmd.emplace(480, &SEPServer::cmdAlreadyInCall);
     _cmd.emplace(500, &SEPServer::cmdLoginFailure);
     _cmd.emplace(650, &SEPServer::cmdListAllLoggedUsers);
 }
@@ -128,6 +127,8 @@ std::string SEPServer::cmdCall(User *user, std::string response)
     ss << "450 ";
     for (int i = 0; i < userList.size(); i++) {
         if (userList[i]->getUserName() == name) {
+            if (userList[i]->isInCall())
+                return ("480");
             ss << port;
             ss << " ";
             ss << user->getUserName();
